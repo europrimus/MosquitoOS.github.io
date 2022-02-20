0d7172f0c1c56e56931e00bb93f6abea3db3d35f
Dorian <doriansomers@gmail.com>
2022-02-20 23:18:25 +0100

    edit homepage + intro docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ import HomepageFeatures from '../components/HomepageFeatures';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+    <header className={clsx('hero hero--primary', styles.heroBanner)} style={{filter: 'sepia(1)'}}>
+      <div className="container" style={{color:'rgb(26,26,26)'}}>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
