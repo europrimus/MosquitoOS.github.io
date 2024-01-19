339a3c73259ab078309c8b20ca256abde56126a2
Dorian S <doriansomers@gmail.com>
2024-01-19 16:53:50 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="http://eepurl.com/hHHDyv">
+            to="https://moskitos.org/#newsletter">
 	    Inscris-toi à la Newsletter            
           </Link>
         </div>
